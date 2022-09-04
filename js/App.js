ec833ca239655ee8391b79290055f795d30c7b63
robin mahmud <robinmahmud358@gmail.com>
2022-09-04 10:02:41 +0600

    added teamperture

--- js/App.js
+++ js/App.js
@@ -1,9 +1,13 @@
 // My Api Don't show anyone
 const Api_key = `a485e8c42b01de8c99d3b405f5ea27be`;
 const loadWeather = (city) => {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}`
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_key}&units=metric`
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => disolayTeamperatur(data))
 }
-loadWeather()
+const disolayTeamperatur = teamPerture => {
+    const containTeamperture = document.getElementById('teamperture');
+    containTeamperture.innerText = teamPerture.main.temp;
+}
+loadWeather('Dhaka');
